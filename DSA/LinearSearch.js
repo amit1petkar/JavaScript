094f44ad0c207a870db38cc7a4f7f433db41c090
Amit Petkar <amit1petkar@gmail.com>
2024-04-02 01:26:25 -0400

    Linear Search

---
+++ DSA/LinearSearch.js
@@ -0,0 +1,71 @@
+/*
+Linear Search: Given an integer array and an element x, find if element is present in array or not. If element is present, then print index of its first occurrence. Else print -1.
+Input: 
+First line contains an integer, the number of test cases ‘T’. Each test case should be an integer. Size of the array ‘n’ in the second line. In the third line, input the integer elements of the array in a single line separated by space. Element X should be input in the fourth line, i.e., after entering the elements of array. Repeat the above steps second line onwards for multiple test cases.
+Output: 
+Print the output in a separate line returning the index of the element X. If the element is not present, then print -1.
+Constraints: 
+1 <= T <= 100 
+1 <= n <= 100 
+1 <= arr[i] <= 100
+
+E.g.
+Input:
+2
+4
+1 2 3 4
+3
+5
+10 90 20 30 40 
+40
+Output:
+2
+4
+*/
+const jsPrompt = require('prompt-sync')({sigint: true});
+
+function validateInput(val) {
+    if(typeof val !== 'number' || val<1 || val>100) {
+        throw "Invalid input"
+    }
+    return val
+}
+
+function linearSearch(arr, searchNum) {
+    for(let i = 0; i<arr.length; i++) {
+        if(arr[i] == searchNum) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+//Enter no. of test cases
+const testCases = validateInput(Number(jsPrompt()))
+const results = []
+
+for(let i=1; i<=testCases; i++) {
+
+    //Enter no. of elements in array
+    let elementsCount = validateInput(Number(jsPrompt()))
+    
+    let arr = []
+
+    //Enter elements in array
+    let elements = String(jsPrompt()).split(' ')
+    
+    if(elements.length !== elementsCount) {
+        throw 'Invalid input'
+    }
+
+    elements.forEach(element => {
+            arr.push(validateInput(Number(element))) //TODO: doesn't throw error
+    });
+
+    //Enter no. to be searched
+    let searchNum = validateInput(Number(jsPrompt()))
+    
+    results.push(linearSearch(arr, searchNum))
+}
+
+results.forEach(result=>console.log(result))
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # JavaScript
+<h2>Initial steps.</h2>
+<ul>
+<li>Ran npm init to create initial draft of package.json.</li>
+<li>Ran node -v > .nvmrc to create .nvmrc. This ensures the desired version (In my case, system's default : v21.7.1) of NodeJs is used. Run nvm use to take effect.</li>
+<li>Installed following packages using npm install
+<ul>
+    <li>prompt-sync (For input prompts).</li>
+
+        // sigint (Signal Interrupt) to true allows user to interrupt (exit) the program flow (using Ctrl+C)
+        const jsPrompt = require('prompt-sync')({sigint: true});
+        let tests = jsPrompt("Enter number:")
+        console.log(tests);
+
+NOTE: NodeJS has in-built functions for the same. They're too wordy.
+
+        const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+        });
+
+        readline.question('Number?', num => {
+        console.log(`Number entered ${num}!`);
+        readline.close();
+        });        
+</ul>
+</li>
+<li>Run code on Terminal
+        
+        node <path to JS file>
+</li>
+
+</ul>
